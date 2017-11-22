363402dc4ea6fe004f9b8ee79bdc71811834437e
sarguments <sarguments@gmail.com>
2017-11-22 19:17:39 +0900

    초기 커밋.

---
+++ StartMoveClient_K2/hoxy_Header.h
@@ -0,0 +1,24 @@
+#pragma once
+
+// 구버전 제외
+#define WIN32_LEAN_AND_MEAN
+
+#pragma comment(lib, "ws2_32")
+
+#include <Windows.h>
+#include <WinSock2.h>
+#include <Ws2tcpip.h>
+
+#include <iostream>
+#include <iomanip>
+#include <locale>
+//#include <locale.h>
+
+using std::wcout;
+using std::wcin;
+using std::endl;
+
+#include "hoxy_CmdStart.h"
+
+//#include "hoxy_TcpSock.h"
+#include "hoxy_SockUtill.h"
